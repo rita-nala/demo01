b47aeb68f0a01af3941d2a8317fd59b420405b4b
rita <986615839@qq.com>
2019-12-31 15:41:33 +0800

    rita第一次提交

---
+++ day48/src/com/rita/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.rita.demo;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		/**
+		 * pwd可以查看当前路径
+		 * git add 文件夹名称，表示把这个文件夹添加到git本地仓库里面去
+		 */
+		System.out.println("hahah ");
+	}
+}
